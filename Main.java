53cdf64f1bb8339979bd4fe17706769e1b43b690
userdawn6 <dawn.chimereze@binghamuni.edu.ng>
2024-04-24 09:23:41 +0100

    Add files via upload
    This program demonstrates a basic example of user authentication with hardcoded username and password values.

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        String username = "dawn";
+        String password = "password";
+
+        if (username.equals("dawn") && password.equals("password")){
+            System.out.println("success login");
+        } else {
+            System.out.println("failed");
+        }
+    }
+}
